a17f0e123bf636feb29baaa69d332fe756dbea55
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-02 22:09:13 +0800

    Firebase configuration

---
+++ public/js/firebase-app-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: 			"AIzaSyBP8q6gRmcjc3g6RKbJB27WfytH4AM2ySE",
+	authDomain: 		"online-ordering-system-35b7e.firebaseapp.com",
+	databaseURL: 		"https://online-ordering-system-35b7e.firebaseio.com",
+	storageBucket: 		"",
+	messagingSenderId: 	"491885107559"
+};
+
+firebase.initializeApp(config);
